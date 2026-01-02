e082acf4ed97352a5e6f0b6b3027cd87c691a958
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-02 18:29:16 +0700

    Tambah fungsi konversi Siswa ke DetailSiswa dan UIStateSiswa

--- app/src/main/java/com/example/questfirebase_073/modeldata/Siswa.kt
+++ app/src/main/java/com/example/questfirebase_073/modeldata/Siswa.kt
@@ -20,3 +20,20 @@ fun DetailSiswa.toDataSiswa(): Siswa = Siswa(
     alamat = alamat,
     telpon = telpon
 )
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
