18756f7e88dc3358384ebd64d633f82e7cc71d5f
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-02 19:06:34 +0700

    Buat DestinasiEntry untuk navigasi ke layar tambah Siswa

---
+++ app/src/main/java/com/example/questfirebase_073/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.questfirebase_073.view.route
+
+import com.example.questfirebase_073.R
+import com.example.questfirebase_073.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
