1f92d61ed8ee0202703df16bfe90d2b448f16991
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-02 19:27:18 +0700

    menggantimain activity

--- app/src/main/java/com/example/questfirebase_073/MainActivity.kt
+++ app/src/main/java/com/example/questfirebase_073/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.questfirebase_073.navigation.DataSiswaApp
 import com.example.questfirebase_073.ui.theme.Questfirebase_073Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             Questfirebase_073Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    // Pastikan composable DataSiswaApp sudah dibuat (biasanya berisi NavHost)
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Questfirebase_073Theme {
-        Greeting("Android")
     }
 }
