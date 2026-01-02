e78d268504d2d64ad668bc9641229fe04bb48fe0
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-02 18:28:47 +0700

    Tambah fungsi untuk mengubah DetailSiswa menjadi Siswa

---
+++ app/src/main/java/com/example/questfirebase_073/modeldata/Siswa.kt
@@ -0,0 +1,22 @@
+package com.example.questfirebase_073.modeldata
+
+data class Siswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+data class DetailSiswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+fun DetailSiswa.toDataSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
