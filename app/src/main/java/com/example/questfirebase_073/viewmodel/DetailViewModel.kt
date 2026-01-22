1ba6d6fa996d53b4ade5d5c237990e34ae5d4b53
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-22 23:49:54 +0700

    konversi id siswa dari String ke Long pada DetailViewModel

--- app/src/main/java/com/example/questfirebase_073/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_073/viewmodel/DetailViewModel.kt
@@ -1,4 +1,36 @@
 package com.example.questfirebase_073.viewmodel
 
-class DetailViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_073.modeldata.Siswa
+import com.example.questfirebase_073.repositori.RepositorySiswa
+import com.example.questfirebase_073.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satuSiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
 }
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    // KUNCI ERROR: ID dikirim sebagai String dari Navigasi, harus di-convert ke Long
+    private val idSiswa: Long = savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+        ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
