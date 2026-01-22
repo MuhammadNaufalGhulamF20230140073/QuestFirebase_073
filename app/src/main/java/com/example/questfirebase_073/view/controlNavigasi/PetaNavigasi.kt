35fe07f34c7edeb78e563dfc52449ca03cc71ee1
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-22 23:47:17 +0700

    implementasi navigasi antar screen siswa

--- app/src/main/java/com/example/questfirebase_073/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/questfirebase_073/view/controlNavigasi/PetaNavigasi.kt
@@ -1,4 +1,28 @@
 package com.example.questfirebase_073.view.controlNavigasi
 
-class PetaNavigasi {
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(navController = navController)
 }
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+                    // Mengirim ID sebagai bagian dari Route
+                    navController.navigate("${DestinasiDetail.route}/$it")
+                }
+            )
